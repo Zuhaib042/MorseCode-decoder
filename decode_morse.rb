65bba859decf94aed8ba943aaf62660d3531b965
Okechukwu Muokwugwo <okei.muokwugwo@gmail.com>
2022-12-15 12:03:07 +0100

    Add decode_word() and decode() methods

--- decode_morse.rb
+++ decode_morse.rb
@@ -12,4 +12,22 @@ def decode_char(char)
   dict.key(char)
 end
 
+# decode words
+def decode_word(str)
+  split_code = str.split
+  empty = ''
+  split_code.each do |ele|
+    empty += decode_char(ele)
+  end
+  empty
+end
+# decode whole sentence
+def decode(str)
+  split_code = str.split('   ')
+  arr = []
+  split_code.each do |ele|
+    arr.push(decode_word(ele))
+  end
+  arr.join(' ')
+end 
 
